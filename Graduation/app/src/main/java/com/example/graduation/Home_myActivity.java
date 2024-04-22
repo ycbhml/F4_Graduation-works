f54de5d20147f9475cc64aba4fde68a156d089f6
yzx <103519629+ycbhml@users.noreply.github.com>
2024-04-23 04:32:44 +0900

    设置界面
    添加了设置界面及其返回和登出按钮

--- Graduation/app/src/main/java/com/example/graduation/Home_myActivity.java
+++ Graduation/app/src/main/java/com/example/graduation/Home_myActivity.java
@@ -2,6 +2,7 @@ package com.example.graduation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,12 +10,12 @@ import com.google.android.material.tabs.TabLayout;
 
 public class Home_myActivity extends AppCompatActivity {
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_my); // 加载布局文件
         // 获取 TabLayout
         TabLayout tabLayout = findViewById(R.id.tabLayout);
-
         // 添加标签
         tabLayout.addTab(tabLayout.newTab().setText("huodong"));
         tabLayout.addTab(tabLayout.newTab().setText("LOL"));
@@ -23,7 +24,6 @@ public class Home_myActivity extends AppCompatActivity {
 
         // 设置默认选中标签为 "My"
         tabLayout.getTabAt(3).select();
-
 
         // 设置标签点击事件
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -37,28 +37,36 @@ public class Home_myActivity extends AppCompatActivity {
                     tabLayout.selectTab(tab);
                     Intent intent = new Intent(Home_myActivity.this, Home_huodongActivity.class);
                     startActivity(intent);
+                    finish();
                 } else if (selectedTabText.equals("LOL")) {// 导航到 Home_lolActivity
                     tabLayout.selectTab(tab);
                     Intent intent = new Intent(Home_myActivity.this, Home_lolActivity.class);
                     startActivity(intent);
+                    finish();
                 } else if (selectedTabText.equals("TFT")) {// 导航到 Home_tftActivity
                     tabLayout.selectTab(tab);
                     Intent intent = new Intent(Home_myActivity.this, Home_tftActivity.class);
                     startActivity(intent);
+                    finish();
                 }
-                // 不执行任何操作，因为点击 "My" 标签时不需要导航
-            }
+                else if (selectedTabText.equals("My")) {
 
+                }// 不执行任何操作，因为点击 "My" 标签时不需要导航
+            }
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
                 // 当标签未被选中时的操作
             }
-
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
                 // 当标签被重新选中时的操作
             }
         });
+    }
+    public void goToSetup(View view) {  //按钮去satup
+        Intent intent = new Intent(Home_myActivity.this, SetupActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
 //    public void goTolol(View view) {
--- Graduation/app/src/main/java/com/example/graduation/SetupActivity.java
+++ Graduation/app/src/main/java/com/example/graduation/SetupActivity.java
@@ -1,23 +1,32 @@
 package com.example.graduation;
 
-import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class SetupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.setup);
     }
+
     public void goToHome(View view) {  //按钮去Home_myActivity
         Intent intent = new Intent(this, Home_myActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToMain(View view) {  //按钮去Home_myActivity
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
