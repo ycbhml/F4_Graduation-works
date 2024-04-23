3e63790bd45e6881664afcb633665289c184fbb9
yzx <103519629+ycbhml@users.noreply.github.com>
2024-04-23 23:19:28 +0900

    简化文件名
    把文件名修改的可读性更高

---
+++ Graduation/app/src/main/java/com/example/graduation/C_lol.java
@@ -0,0 +1,28 @@
+package com.example.graduation;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class C_lol extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.c_lol);
+
+    }
+}
+//    public void goTohuodong(View view) {
+//        Intent intent = new Intent(this, Home_huodongActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//    public void goTotft(View view) {
+//        Intent intent = new Intent(this, Home_tftActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//    public void goTomy(View view) {
+//        Intent intent = new Intent(this, Home_myActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
