ebd6b0f3b8b53a94cf03482d98d9444fb55be6e8
RosmontisQD <1264376147@qq.com>
2024-08-30 21:15:09 +0900

    upate
     update for first login page

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import Index from './src/screen/login'
+
+
+export default class App extends Component {
+  render() {
+    return (
+        <Index />
+    )
+  }
+}
